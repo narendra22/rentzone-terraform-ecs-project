b953ebe0473a90df4898532e9921b58031bb8728
narendra26 <narendra.leander22@gmail.com>
2024-07-09 19:26:33 -0400

    created vpc file

--- variables.tf
+++ variables.tf
@@ -16,3 +16,42 @@ variable "environment" {
     type        = string 
   
 }
+
+# VPC variables
+variable "vpc_cidr" {
+    description = "vpc cidr block"
+    type = string
+}
+
+
+
+variable "public_subnet_az1_cidr" {
+    description = "public subnet az1 cidr block"
+    type = string
+}
+
+variable "public_subnet_az2_cidr" {
+    description = "public subnet az2 cidr block"
+    type = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+    description = "private app subnet az1 cidr block"
+    type = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+    description = "private app subnet az2 cidr block"
+    type = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+    description = "private data subnet az1 cidr block"
+    type = string
+}
+
+
+variable "private_data_subnet_az2_cidr" {
+    description = "private data subnet az2 cidr block"
+    type = string
+}
